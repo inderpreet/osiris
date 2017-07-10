50af6cc6b94bcc469eb59987140b0a43038dfb27
Inderpreet Singh <er.inderpreet@gmail.com>
2017-07-10 19:49:19 +0530

    Added a localhost:8080/hello REST handler. Also got the build thing working properly.

---
+++ server/src/main/java/in/co/osiris/server/HomeController.java
@@ -0,0 +1,13 @@
+package in.co.osiris.server;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hey Man! Wassup?";
+    }
+}
+
